4d8311962a6b5d4ec1daf3e7959b008e0202c4e6
xNtrow <xntrowdev@gmail.com>
2022-08-08 13:45:18 +0200

    Update

--- src/main/kotlin/io/github/axst/ClientCore.kt
+++ src/main/kotlin/io/github/axst/ClientCore.kt
@@ -1,12 +1,9 @@
 package io.github.axst
 
-import lombok.Getter
-
 class ClientCore {
 
     companion object Instances {
         @JvmStatic
-        @Getter
         val instance: ClientCore = ClientCore()
     }
 
